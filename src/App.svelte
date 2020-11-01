aa674d96f7af3320234388fdc2a2c09f9359d0e4
Gilles CRUCHON <gilles.cruchon@gmail.com>
2020-11-01 16:33:34 +0100

    feat(game): give a choice play/host at startup

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { FirebaseApp, User, Doc, Collection } from "sveltefire";
+  import { FirebaseApp, User } from "sveltefire";
   import firebase from "firebase/app";
   import "firebase/firestore";
   import "firebase/auth";
@@ -8,15 +8,20 @@
 
   import firebaseConfig from "./config/firebaseConfig";
 
-  import ShowUser from "./components/user/ShowUser.svelte"
-  import SignIn from "./components/user/SignIn.svelte"
-  import GameHeader from "./components/game/GameHeader.svelte"
-  import GameStart from "./components/game/GameStart.svelte"
-  import GamePlayers from "./components/game/GamePlayers.svelte"
-  import QuestionHeader from "./components/question/QuestionHeader.svelte"
-  import QuestionCountDown from "./components/question/QuestionCountDown.svelte"
+  import ShowUser from "./components/user/ShowUser.svelte";
+  import SignIn from "./components/user/SignIn.svelte";
+  import GameHost from "./components/game/GameHost.svelte";
+  import GameStart from "./components/game/GameStart.svelte";
+  import GamePlayers from "./components/game/GamePlayers.svelte";
+  import QuestionHeader from "./components/question/QuestionHeader.svelte";
+  import QuestionCountDown from "./components/question/QuestionCountDown.svelte";
 
   firebase.initializeApp(firebaseConfig);
+
+  $: userType = "";
+  let updateUserType = (type) => {
+    userType = type;
+  };
 </script>
 
 <!-- Styles -->
@@ -49,7 +54,6 @@
 <main>
   {#if !firebaseConfig.projectId}
     <strong>Please check your config...</strong>
-    .
   {/if}
 
   <!-- 1. 🔥 Firebase App -->
@@ -58,69 +62,39 @@
 
     <!-- 2. 😀 Get the current user -->
     <User let:user let:auth>
-      <ShowUser user={user} auth={auth}/>
-      
+      <ShowUser {user} {auth} />
+
       <div slot="signed-out">
-        <SignIn auth={auth}/>
+        <SignIn {auth} />
       </div>
 
       <hr />
 
-      <!-- 3. 📜 Get a Firestore document owned by a user -->
-      <Doc path={`games/${user.uid}`} let:data={game} let:ref={gameRef} log>
-        <GameHeader id={game.id} createdAt={game.createdAt} />
+      {#if userType === 'host'}
+        <GameHost userId={user.uid} />
+      {:else if userType === 'player'}
+        <p>Player</p>
+      {:else}
+        <h2>Que souhaitez-vous faire ?</h2>
 
-        <span slot="loading">Chargement du jeu...</span>
-        <span slot="fallback">
-          <GameStart gameRef={gameRef} />
-        </span>
+        <p>
+          <button
+            on:click={() => {
+              updateUserType('host');
+            }}>
+            Initier un nouveau jeu
+          </button>
+        </p>
+        <p>
+          <button
+            on:click={() => {
+              updateUserType('player');
+            }}>
+            Rejoindre un jeu existant
+          </button>
+        </p>
+      {/if}
 
-        <!-- WAITING FOR PLAYERS -->
-        {#if game.state === 'waitingForPlayers'}
-          <GamePlayers gameRef={gameRef} />
-
-        <!-- COUNTDOWN BEFORE QUESTION -->
-        {:else if game.state === 'preQuestion'}
-          <QuestionHeader game={game} />
-          <QuestionCountDown gameRef={gameRef} numberOfSeconds={5} nextState="question" />
-        <!-- ASKING THE QUESTION -->
-        {:else if game.state === 'question'}
-          <QuestionHeader game={game} />
-          <QuestionCountDown gameRef={gameRef} numberOfSeconds={30} nextState="showResults" />
-          <p>Texte de la question</p>
-        <!-- SHOW WHAT PEOPLE ANSWERED -->
-        {:else if game.state === 'showResults'}
-          <h3>showResults</h3>
-          <QuestionHeader game={game} />
-          <button
-            on:click={() => gameRef.update({
-              state: 'showAnswer',
-              })}>
-            Voir la réponse!
-          </button>
-        <!-- SHOW THE ACTUAL CORRECT ANSWER -->
-        {:else if game.state === 'showAnswer'}
-          <h3>answer</h3>
-          <QuestionHeader game={game} />
-          <button
-            on:click={() => gameRef.update({
-              state: 'leaderboard',
-              })}>
-            Voir le classement!
-          </button>
-        <!-- SHOW THE LEADERBOARD -->
-        {:else if game.state === 'leaderboard'}
-          <h3>Classement</h3>
-          <QuestionHeader game={game} />
-          <button
-            on:click={() => gameRef.update({
-              state: 'preQuestion',
-              currentQuestionIndex: game.currentQuestionIndex + 1,
-              })}>
-            Prochaine question!
-          </button>
-        {/if}
-      </Doc>
     </User>
   </FirebaseApp>
 </main>
---
+++ src/components/game/GameHost.svelte
@@ -0,0 +1,60 @@
+<script>
+  import { Doc } from "sveltefire";
+
+  import GameHeader from "./GameHeader.svelte";
+  import GameStart from "./GameStart.svelte";
+  import GamePlayers from "./GamePlayers.svelte";
+  import QuestionHeader from "../question/QuestionHeader.svelte";
+  import QuestionCountDown from "../question/QuestionCountDown.svelte";
+
+  export let userId;
+</script>
+
+<Doc path={`games/${userId}`} let:data={game} let:ref={gameRef} log>
+  <GameHeader id={game.id} createdAt={game.createdAt} />
+
+  <span slot="loading">Chargement du jeu...</span>
+  <span slot="fallback">
+    <GameStart {gameRef} />
+  </span>
+
+  <!-- WAITING FOR PLAYERS -->
+  {#if game.state === 'waitingForPlayers'}
+    <GamePlayers {gameRef} />
+
+    <!-- COUNTDOWN BEFORE QUESTION -->
+  {:else if game.state === 'preQuestion'}
+    <QuestionHeader {game} />
+    <QuestionCountDown {gameRef} numberOfSeconds={5} nextState="question" />
+    <!-- ASKING THE QUESTION -->
+  {:else if game.state === 'question'}
+    <QuestionHeader {game} />
+    <QuestionCountDown {gameRef} numberOfSeconds={30} nextState="showResults" />
+    <p>Texte de la question</p>
+    <!-- SHOW WHAT PEOPLE ANSWERED -->
+  {:else if game.state === 'showResults'}
+    <h3>showResults</h3>
+    <QuestionHeader {game} />
+    <button on:click={() => gameRef.update({ state: 'showAnswer' })}>
+      Voir la réponse!
+    </button>
+    <!-- SHOW THE ACTUAL CORRECT ANSWER -->
+  {:else if game.state === 'showAnswer'}
+    <h3>answer</h3>
+    <QuestionHeader {game} />
+    <button on:click={() => gameRef.update({ state: 'leaderboard' })}>
+      Voir le classement!
+    </button>
+    <!-- SHOW THE LEADERBOARD -->
+  {:else if game.state === 'leaderboard'}
+    <h3>Classement</h3>
+    <QuestionHeader {game} />
+    <button
+      on:click={() => gameRef.update({
+          state: 'preQuestion',
+          currentQuestionIndex: game.currentQuestionIndex + 1,
+        })}>
+      Prochaine question!
+    </button>
+  {/if}
+</Doc>
