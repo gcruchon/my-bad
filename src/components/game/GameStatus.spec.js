305683c86d0e9afdd0d6ef571a111282d28db69d
Gilles CRUCHON <gilles.cruchon@gmail.com>
2020-11-11 18:47:54 +0100

    test(game): test status

---
+++ src/components/game/GameStatus.spec.js
@@ -0,0 +1,49 @@
+import { render, fireEvent } from '@testing-library/svelte';
+import GameStatus from './GameStatus.svelte';
+
+const gameNotStarted = {
+  state: 'waitingForPlayers',
+  currentQuestionIndex: 0,
+  questions: [1, 2, 3],
+};
+const gameStarted = {
+  currentQuestionIndex: 1,
+  questions: [1, 2, 3],
+};
+
+describe('GameButtons', () => {
+  it('should fail beautifully game has an unknown state', async () => {
+    const strangeGame = {
+      state: 'WTF',
+      ...gameStarted
+    }
+    const { container } = render(GameStatus, {
+      props: {
+        game: strangeGame,
+      },
+    });
+    expect(container).toContainHTML('Statut inconnu "WTF"');
+  });
+  it('should show a disclaimer when game is not started', async () => {
+    const { container } = render(GameStatus, {
+      props: {
+        game: gameNotStarted,
+      },
+    });
+    expect(container).toContainHTML('En attente de joueurs');
+  });
+  ['preQuestion', 'question', 'showResults', 'showAnswer'].forEach(state => {
+    it(`should show question number when status is '${state}'`, async () => {
+      const game = {
+        state,
+        ...gameStarted
+      }
+      const { container } = render(GameStatus, {
+        props: {
+          game,
+        },
+      });
+      expect(container).toContainHTML('Question n°2');
+    });
+  });
+});
--- src/components/game/GameStatus.svelte
+++ src/components/game/GameStatus.svelte
@@ -6,4 +6,4 @@
   En attente de joueurs
 {:else if game.state == 'preQuestion' || game.state == 'question' || game.state == 'showResults' || game.state == 'showAnswer'}
   Question n°{game.currentQuestionIndex + 1}
-{:else}Statut inconnu "{game.state}""{/if}
+{:else}Statut inconnu "{game.state}"{/if}
