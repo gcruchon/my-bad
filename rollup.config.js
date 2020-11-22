7f9b6df2a73b87b673a39e4cc45571673c8e5e7c
Gilles CRUCHON <gilles.cruchon@gmail.com>
2020-11-22 21:29:04 +0100

    chore(rollup): fix build

--- rollup.config.js
+++ rollup.config.js
@@ -120,11 +120,13 @@ function generateIndexHtml() {
 <html lang="en">
 	<head>
 	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
 
 	<title>My Bad!</title>
 
-	<link rel="icon" type="image/png" href="/favicon.ico" />
+  <link rel="icon" type="image/png" href="/favicon.ico" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" integrity="sha256-BJ/G+e+y7bQdrYkS2RBTyNfBHpA9IuGaPmf9htub5MQ=" crossorigin="anonymous" />
 	<link rel="stylesheet" href="/global.css" />
 	<link rel="stylesheet" href="/bundle.${hash}.css" />
 
